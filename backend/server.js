1bed990c05c52e2c531bb642a53e73ecdb24d830
Sravan <78921929+kumar-sravan-au17@users.noreply.github.com>
2021-11-27 02:09:52 +0530

    added deployement details

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const noteRoutesObj = require('./routes/note_router')
 const userRoutes = require('./routes/user_router')
 const morgan = require('morgan')
 const cookieParser = require('cookie-parser')
+const {resolve} = require('path')
 
 const PORT = process.env.PORT || 3000
 const db_url = process.env.DB_URL
@@ -15,8 +16,16 @@ app.use(cookieParser())
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
+const buildFolder = resolve(__dirname, '../frontend/build')
+app.use(express.static(buildFolder))
+
 app.use('/api/notes', noteRoutesObj)
 app.use('/api/', userRoutes)
+
+app.get("*" , (req, res) => {
+    res.sendFile(`${buildFolder}/index.html`)
+})
+
 
 async function dbConnect() {
     await mongoose.connect(db_url)
